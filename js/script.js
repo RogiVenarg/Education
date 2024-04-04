b91b91794ef7b3493f5b4a2b70170e4580493158
RogiVenarg <kubopr@inbox.ru>
2024-04-04 14:02:36 +0300

    Создание наверно окончательной HTML структуры. Спривязка пустых функций к кнопкам.

---
+++ js/script.js
@@ -0,0 +1,39 @@
+function Zadanie1(){
+	
+}
+
+function Zadanie2(){
+	
+}
+
+function Zadanie3(){
+	
+}
+
+function Zadanie4(){
+	
+}
+
+function Zadanie5(){
+	
+}
+
+function Zadanie6(){
+	
+}
+
+function Zadanie7(){
+	
+}
+
+function Zadanie8(){
+	
+}
+
+function Zadanie9(){
+	
+}
+
+function Zadanie10(){
+	
+}
