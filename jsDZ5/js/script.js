5eba3d5f9ebe89ad3777fdc5c747ca5865bf37ca
RogiVenarg <kubopr@inbox.ru>
2024-04-08 14:45:50 +0300

    Создал рыбу для ДЗ 5 по js

---
+++ jsDZ5/js/script.js
@@ -0,0 +1,23 @@
+function Zadanie1(){
+	alert("1 работает");
+}
+
+function Zadanie2(){
+	alert("2 работает");
+}
+
+function Zadanie3(){
+	alert("3 работает");
+}
+
+function Zadanie4(){
+	alert("4 работает");
+}
+
+function Zadanie5(){
+	alert("5 работает");
+}
+
+function Zadanie6(){
+	alert("6 работает");
+}
