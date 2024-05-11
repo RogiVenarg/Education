36abdc7aae6868cbf8c1ff163d02c220c05dbb8b
RogiVenarg <kubopr@inbox.ru>
2024-05-11 21:56:30 +0300

    Начал делать ДЗ 7

---
+++ jsDZ7/js/script.js
@@ -0,0 +1,55 @@
+let gpElement=document.getElementsByClassName("rezults");
+
+for (var i = 0; i < gpElement.length; i++){
+	gpElement[i].addEventListener("click", function(){
+		this.classList.add("MyNewclass");
+	})	
+}
+
+// 1 задание		
+function Zadanie1_1(){
+	alert(11);
+}
+function Zadanie1_2(){
+	alert(12);
+}
+function Zadanie1_3(){
+	alert(13);
+}
+
+// 2 задание
+function Zadanie2_1(){
+	alert(21);
+}
+function Zadanie2_2(){
+	alert(22);
+}
+function Zadanie2_3(){
+	alert(23);
+}
+function Zadanie2_4(){
+	alert(24);
+}
+
+// 3 задание
+function Zadanie3_1(){
+	alert(31);
+}
+
+
+// 4 задание
+function Zadanie4_1(){
+	alert(41);
+}
+function Zadanie4_2(){
+	alert(42);
+}
+function Zadanie4_3(){
+	alert(43);
+}
+function Zadanie4_4(){
+	alert(44);
+}
+function Zadanie4_5(){
+	alert(45);
+}
