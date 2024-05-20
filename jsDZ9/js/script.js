b54edbc9dd036abe0b8c10a4f03e277f1e280c22
RogiVenarg <kubopr@inbox.ru>
2024-05-20 22:44:59 +0300

    Начал делать ДЗ 9

---
+++ jsDZ9/js/script.js
@@ -0,0 +1,28 @@
+let gpElement=document.getElementsByClassName("rezults");
+
+for (var i = 0; i < gpElement.length; i++){
+	gpElement[i].addEventListener("click", function(){
+		this.classList.add("MyNewclass");
+	})	
+}
+
+// 1 задание
+function Zadanie1(){
+	alert("У тебя не плохо получается 1");
+}
+
+// 2 задание
+function Zadanie2(){
+	alert("У тебя не плохо получается 2");
+}
+
+// 3 задание
+function Zadanie3(){
+	alert("У тебя не плохо получается 3");
+}
+
+// 4 задание
+
+function Zadanie4(){
+	alert("У тебя не плохо получается 4");
+}
