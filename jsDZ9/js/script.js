77b546874edb86c31bbed99eab71303ce21c4b40
RogiVenarg <kubopr@inbox.ru>
2024-05-23 13:40:41 +0300

    сделал 2 задание ДЗ 9

--- jsDZ9/js/script.js
+++ jsDZ9/js/script.js
@@ -45,8 +45,133 @@ function Zadanie1(){
 }
 
 // 2 задание
+class HtmlElement {
+	#myAtributes = [];
+	#myStyle = [];
+	#myChildTag = [];
+	
+	constructor(nameOfTag, autoClose, myTxt){
+		this.nameOfTag = nameOfTag;
+		this.autoClose = autoClose;
+		this.myTxt = myTxt;
+	}
+	//---=== Атрибуты ===---
+	get myAtributes(){
+		let str = "";
+		
+		for (let i = 0; i < this.#myAtributes.length; i++){
+			if (i !== 0) str += " ";
+			str += this.#myAtributes[i];
+		}
+		return str;
+	}
+	set myAtributes(val){
+		if ((val.length == 0) || (val.length > 2)) return undefined;
+		else this.#myAtributes[this.#myAtributes.length] = val[0] + '="' + val[1] + '"';		
+	}
+	delMyAtribut = function(ind){
+		console.log(ind);
+		ind = Number(ind);
+		console.log(ind, this.#myAtributes.length - 1);
+		if ((isNaN(ind)) || (ind < 0) || (ind > this.#myAtributes.length - 1)) return undefined;
+		else this.#myAtributes.splice(ind, 1);
+	}
+	pushMyAtribut = function(name, val){
+		this.#myAtributes.push(name + '="' + val + '"');
+	}
+	//---=== Стили ===---
+	get myStyle(){
+		let str = 'style="';
+		
+		for (let i = 0; i < this.#myStyle.length; i++){
+			if (i !== 0) str += " ";
+			str += this.#myStyle[i];
+		}
+		str = str + '"';
+		return str;
+	}
+	set myStyle(val){
+		if ((val.length == 0) || (val.length > 2)) return undefined;
+		else this.#myStyle[this.#myStyle.length] = val[0] + ': ' + val[1] + ';';		
+	}
+	delMyStyle = function(ind){
+		ind = Number(ind);
+		if ((isNaN(ind)) || (ind < 0) || (ind > this.#myStyle.length - 1)) return undefined;
+		else this.#myStyle.splice(ind, 1);
+	}
+	pushMyStyle = function(name, val){
+		this.#myStyle.push(name + ': ' + val + ';');
+	}
+	
+	
+	//---=== Дочерние ===---
+	delMyChildTag = function(ind){
+		ind = Number(ind);
+		if ((isNaN(ind)) || (ind < 0) || (ind > this.#myChildTag.length - 1)) return undefined;
+		else this.#myChildTag.splice(ind, 1);
+	}
+	pushMyChildTag = function(obj){
+		if (typeof obj !== 'object') return undefined;
+		this.#myChildTag.push(obj);
+	}
+	unshiftMyChildTag = function(obj){
+		if (typeof obj !== 'object') return undefined;
+		this.#myChildTag.unshift(obj);
+	}
+	
+	//---=== getHtml() ===---
+	getHtml = function(){
+		let str = "<";
+		let i;
+		str += this.nameOfTag + " " + this.myStyle + " " + this.myAtributes + ">" + this.myTxt;
+		
+		// str += "str вложенных тэгов";
+		
+		for (i = 0; i < this.#myChildTag.length; i++){
+			str += this.#myChildTag[i].getHtml();
+		}
+		
+		if (!this.autoClose) str += "</" + this.nameOfTag + ">";
+		return str;
+	}
+}
+
+let tagA = new HtmlElement("a", false, "More...");
+tagA.myAtributes = ["href", "https://www.lipsum.com/"];
+tagA.pushMyAtribut("target", "_blank");
+let tagP = new HtmlElement("p", false, '"Lorem Ipsum is simply dummy text ' + 
+			`of the printing and typesetting industry. Lorem Ipsum has been ` + 
+			`the industy's standart dummy text ever since the 1500s, when an ` + 
+			`unknown printer took a galley of type and scrambled it to make ` + 
+			`a type specimen book."`);
+tagP.pushMyStyle("text-align", "justify");
+tagP.pushMyChildTag(tagA);
+let tagImg = new HtmlElement("img", true, "");
+tagImg.pushMyStyle("width", "100%");
+tagImg.pushMyAtribut("scr", "lipsum.jpg");
+tagImg.pushMyAtribut("alt", "Lorem Ipsum");
+let tagH3 = new HtmlElement("h3", false, "What is Lorem Ipsum?");
+let tagDiv2 = new HtmlElement("div", false, "");
+tagDiv2.pushMyStyle("width", "300px");
+tagDiv2.pushMyStyle("margin", "10px");
+tagDiv2.pushMyChildTag(tagImg);
+tagDiv2.pushMyChildTag(tagP);
+tagDiv2.unshiftMyChildTag(tagH3);
+let tagDiv1 = new HtmlElement("div", false, "");
+tagDiv1.pushMyAtribut("id", "wrapper");
+tagDiv1.pushMyStyle("display", "flex");
+tagDiv1.pushMyChildTag(tagDiv2);
+tagDiv1.pushMyChildTag(tagDiv2);
+
 function Zadanie2(){
-	alert("У тебя не плохо получается 2");
+	let newWinWid = window.innerWidth * 0.7;
+	let newWinHei = window.innerHeight * 0.7;
+	let newWinLeft = (window.innerWidth - newWinWid) / 2  ;
+	let newWinTop = (window.innerHeight - newWinHei)/ 2 ;
+	let newWindow = window.open("", null ,"popup = true, width = " + newWinWid +
+					", height = " + newWinHei +", left = " + newWinLeft +", top = "+newWinTop);
+	newWindow.document.write(tagDiv1.getHtml());
+	newWindow.alert("Изображения в задании нету, поэтому оно не загружено :)")
 }
 
 // 3 задание
